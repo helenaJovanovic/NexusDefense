700df6c50f7744e3167e27df8c4274ee39c1eac9
Logan <viktorgizdavic245@gmail.com>
2020-11-27 18:27:54 +0100

    json parsing updates

--- code/src/SpriteLoader.cpp
+++ code/src/SpriteLoader.cpp
@@ -1,11 +1,13 @@
 #include <code/include/SpriteLoader.hpp>
-/*#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>*/
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 #include <QDirIterator>
 #include <QDebug>
+#include <iostream>
+#include <sstream>
 
-//namespace pt = boost::property_tree
+namespace pt = boost::property_tree;
 
 SpriteLoader::SpriteLoader(
     const QString &unitsFolderPath,
@@ -32,7 +34,23 @@ QMap<QString, Sprite*> SpriteLoader::parseAndLoadSprites(const QString& path) {
 }
 
 Sprite *SpriteLoader::parseSprite(const QString &filePath) {
-    QString name;
-    qDebug() << "Currently parsing: " << filePath;
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        throw std::exception();
+    QTextStream in(&file);
+
+    std::stringstream ss;
+    ss << in.readAll().toStdString();
+
+    pt::ptree tree;
+    pt::read_json(ss, tree);
+
+    // TODO: use boost to parse through json files and
+    // create a Sprite object based on the info in the file
+    QString name = QString::fromStdString(tree.get("name", ""));
+    QString initialState = QString::fromStdString(tree.get("spriteAnimation.initialState", ""));
+
+    qDebug() << name << ", initial state: " << initialState;
+
     return new Sprite(name);
 }
